627aefb3c549e47135ca2439f9ec59d69ade9781
Linette Hsu <linettehsu@Linettes-MacBook-Air.local>
2017-02-28 17:07:25 -0600

    adding working game files

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+	var randomNumber = 0;
+	var score = 0;
+	var wins = 0;
+	var losses = 0;
+
+	function getCrystalValue() {
+		return Math.floor(Math.random() * (12 - 1) + 1);
+	};
+
+	function start() {
+		randomNumber = Math.floor(Math.random() * (120 - 19) + 19);
+		score = 0;
+		$("#score").text(score);
+		$("#randomNumber").text(randomNumber);
+		$("#emerald").data("value", getCrystalValue());
+		$("#ruby").data("value", getCrystalValue());
+		$("#sapphire").data("value", getCrystalValue());
+		$("#topaz").data("value", getCrystalValue());
+	};
+
+	start();
+
+	$(".crystal").on("click", function () {
+		score += $(this).data("value");
+		$("#score").text(score);
+
+		console.log(this)
+
+		if (score === randomNumber) {
+			wins++;
+			$("#wins").text(wins);
+			start();
+		} else if (score > randomNumber) {
+			losses++;
+			$("#losses").text(losses);
+			start();
+		}
+	}); 
+
+});
